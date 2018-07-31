fa9284e40ccb163850f2d4f43eab574c945fe744
LeonardoMinniti <33843867+LeonardoMinniti@users.noreply.github.com>
2018-07-31 17:48:35 +0200

    Initial commit

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static int round;
+    private bool winner;
+    public SpriteRenderer[] tiles;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+        if (!winner)
+        {
+            if (tiles[0].sprite == tiles[1].sprite && tiles[0].sprite == tiles[2].sprite && tiles[0].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+
+            }else if(tiles[0].sprite == tiles[3].sprite && tiles[0].sprite == tiles[6].sprite && tiles[0].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if (tiles[0].sprite == tiles[4].sprite && tiles[0].sprite == tiles[8].sprite && tiles[0].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if(tiles[3].sprite == tiles[4].sprite && tiles[3].sprite == tiles[5].sprite && tiles[3].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if(tiles[6].sprite == tiles[7].sprite && tiles[6].sprite == tiles[8].sprite && tiles[6].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if(tiles[2].sprite == tiles[4].sprite && tiles[2].sprite == tiles[6].sprite && tiles[2].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if(tiles[1].sprite == tiles[4].sprite && tiles[1].sprite == tiles[7].sprite && tiles[1].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }else if(tiles[2].sprite == tiles[5].sprite && tiles[2].sprite == tiles[8].sprite && tiles[2].sprite != null)
+            {
+                Debug.Log("You win");
+                winner = true;
+            }
+            if(round == 9)
+            {
+                Debug.Log("Draw");
+            }
+            if (winner)
+            {
+                if(round % 2 != 0)
+                {
+                    Debug.Log("Player1 Wins");
+                }
+                else
+                {
+                    Debug.Log("Player2 Wins");
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ Assets/Script/Mark.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Mark : MonoBehaviour {
+    public SpriteRenderer X;
+    public SpriteRenderer O;
+    public SpriteRenderer box;
+
+    public void OnMouseDown()
+    {
+        if(box.sprite == null)
+        {
+            if (GameManager.round % 2 == 0)
+            {
+                box.sprite = X.sprite;
+                Debug.Log("X");
+                GameManager.round++;
+            }
+            else
+            {
+                box.sprite = O.sprite;
+                Debug.Log("O");
+                GameManager.round++;
+            }
+        }
+        else
+        {
+            Debug.Log("this slot is already Taken");
+        }
+        
+        //else if(itsPlayer2)
+        //box.sprite = O.sprite;
+        //Debug.Log("O")
+    }
+}
